6cff5d4cbf6af9404921d9a18eda1b75af74b759
akianandam1 <62275636+akianandam1@users.noreply.github.com>
2022-08-26 15:02:44 +0000

    Update multipleperiods2.py

--- multipleperiods2.py
+++ multipleperiods2.py
@@ -36,109 +36,46 @@ def optimize(vec, m_1, m_2, m_3, time_step, max_period, num_periods, num_epochs,
     
     optimizer.zero_grad()
     i = 0
-    global period_index
-    while i < num_epochs/num_periods:
-        
-        
-        if i == 0:
-            input_vec = torch.cat((vec, torch.tensor([m_1,m_2,m_3])))
-            data_set = torchstate(input_vec, time_step, max_period, method)
-            print(f"Max Period: {max_period}")
-            first_index = nearest_position_state(1, data_set[0], data_set, 300, len(data_set), time_step)
-            first_particle_state = data_set[first_index]
-            second_index = nearest_position_state(2, data_set[0], data_set, 300, len(data_set), time_step)
-            second_particle_state = data_set[second_index]
-            third_index = nearest_position_state(3, data_set[0], data_set, 300, len(data_set), time_step)
-            third_particle_state = data_set[third_index]
-            loss = nearest_position(1, data_set[0], first_particle_state) + nearest_position(2, data_set[0],
-                                                                                                 second_particle_state) + nearest_position(
-                3, data_set[0], third_particle_state)
-            
-            with open("optimizelog2.txt", "a") as file:
-                file.write(f"{input_vec},{i},{loss}\\n")
-        
-            print(loss)
-
-            loss.backward()
-            
-            # Updates input vector
-            optimizer.step()
-            print(input_vec)
-            print(f"Epoch: {i}")
-            optimizer.zero_grad()
-            period_index = int((first_index + second_index + third_index) / 3)
-            i += 1              
-            
-        elif i == 1:
-         
-            input_vec = torch.cat((vec, torch.tensor([m_1,m_2,m_3])))
-            data_set = torchstate(input_vec, time_step, max_period, method)
-            print(f"Max Period: {max_period}")
-            first_index = nearest_position_state(1, data_set[0], data_set, 300, period_index+100, time_step)
-            first_particle_state = data_set[first_index]
-            second_index = nearest_position_state(2, data_set[0], data_set, 300, period_index+100, time_step)
-            second_particle_state = data_set[second_index]
-            third_index = nearest_position_state(3, data_set[0], data_set, 300, period_index+100, time_step)
-            third_particle_state = data_set[third_index]
-            loss = nearest_position(1, data_set[0], first_particle_state) + nearest_position(2, data_set[0],
-                                                                                                 second_particle_state) + nearest_position(
-                3, data_set[0], third_particle_state)
-                
-            period_index = int((first_index + second_index + third_index) / 3)
-            with open("optimizelog2.txt", "a") as file:
-                file.write(f"{input_vec},{i},{loss}\\n")
-        
-            print(loss)
-          
-            loss.backward()
-            
-            # Updates input vector
-            optimizer.step()
-            print(input_vec)
-            print(f"Epoch: {i}")
-            optimizer.zero_grad()
-            s2 = time.time()
-       
-            i += 1
-
-                        
-        else:
-            k = 0
-            while k < num_periods:
-                input_vec = torch.cat((vec, torch.tensor([m_1,m_2,m_3])))
-                data_set = torchstate(input_vec, time_step, max_period, method)
-                print(f"Max Period: {max_period}")
-                first_index = nearest_position_state(1, data_set[0], data_set, k*period_index+300, (k+1)*(period_index+100), time_step)
-                first_particle_state = data_set[first_index]
-                second_index = nearest_position_state(2, data_set[0], data_set, k*period_index+300, (k+1)*(period_index+100), time_step)
-                second_particle_state = data_set[second_index]
-                third_index = nearest_position_state(3, data_set[0], data_set, k*period_index+300, (k+1)*(period_index+100), time_step)
-                third_particle_state = data_set[third_index]
-                loss = nearest_position(1, data_set[0], first_particle_state) + nearest_position(2, data_set[0], second_particle_state) + nearest_position(3, data_set[0], third_particle_state)
-                
-                with open("optimizelog2.txt", "a") as file:
-                    file.write(f"{input_vec},{i},{loss}\\n")
-        
-                print(loss)
-
-                loss.backward()
+    period_index = int(5.6664*time_step)
+    while i < num_epochs:
               
-                # Updates input vector
-                optimizer.step()
-                print(input_vec)
-                print(f"Epoch: {i}")
-                optimizer.zero_grad()
-                k += 1
-            i += num_periods
+        k = num_periods
+        input_vec = torch.cat((vec, torch.tensor([m_1,m_2,m_3])))
+        data_set = torchstate(input_vec, time_step, max_period, method)
+        print(f"Max Period: {max_period}")
+        first_index = nearest_position_state(1, data_set[0], data_set, k*period_index+300, (k+1)*(period_index+100), time_step)
+        first_particle_state = data_set[first_index]
+        second_index = nearest_position_state(2, data_set[0], data_set, k*period_index+300, (k+1)*(period_index+100), time_step)
+        second_particle_state = data_set[second_index]
+        third_index = nearest_position_state(3, data_set[0], data_set, k*period_index+300, (k+1)*(period_index+100), time_step)
+        third_particle_state = data_set[third_index]
+        loss = nearest_position(1, data_set[0], first_particle_state) + nearest_position(2, data_set[0], second_particle_state) + nearest_position(3, data_set[0], third_particle_state)
+
+        with open("optimizelog2.txt", "a") as file:
+            file.write(f"{input_vec},{i},{loss}\\n")
+
+        print(loss)
+
+        loss.backward()
+
+        # Updates input vector
+        optimizer.step()
+        print(input_vec)
+        print(f"Epoch: {i}")
+        optimizer.zero_grad()  
+            
+            
+               
+        i += num_periods
         
         
 m_1 = 1
 m_2 = 1
 m_3 = 0.75
-vec = torch.tensor([-0.9819,  0.0309,  0.0000,  0.9664,  0.0286,  0.0000, -0.0131,  0.0096,
-         0.0000,  0.4220,  0.2533,  0.0000,  0.4212,  0.2707,  0.0000, -1.1225,
+vec = torch.tensor([-0.9819,  0.0308,  0.0000,  0.9665,  0.0287,  0.0000, -0.0131,  0.0096,
+         0.0000,  0.4220,  0.2533,  0.0000,  0.4213,  0.2707,  0.0000, -1.1225,
         -0.6964,  0.0000], requires_grad = True)
 
 optimizer = torch.optim.NAdam([vec], lr=.0001)
-optimize(vec, m_1, m_2, m_3, .0008, 25, 4, 520, "rk4", optimizer)
+optimize(vec, m_1, m_2, m_3, .0008, 25, 4, 520, "dopri5", optimizer)
     
